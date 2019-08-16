890290bb58cd17a16fedac547cabf77a12042729
Rehman Zafar <m.rehmanzafar@hotmail.com>
2019-08-15 23:12:41 -0400

    Update xai-iml-sota.R

--- xai-iml-sota.R
+++ xai-iml-sota.R
@@ -2,6 +2,6 @@
 library("aRxiv")
 
 # interpretability in the stat.* and cs.* 
-results <- arxiv_search(query = 'ti:visualization analytics AND (cat:stat* OR cat:cs*)',  
+results <- arxiv_search(query = 'ti:interpretable AND (cat:stat* OR cat:cs*)',  
                   limit=100, sort_by = "submitted", ascending = FALSE) # AND submittedDate:[201906240000 TO 201906252400]',
 results[,c("id", "submitted", "title")]
